5c4214c4ebf030027607b76bf4fc719cd4163fce
c17 <ishugodhani08@gmail.com>
2023-06-12 17:31:00 +0530

    4

--- app/src/main/java/com/example/firebase_auth/ProductList.java
+++ app/src/main/java/com/example/firebase_auth/ProductList.java
@@ -2,9 +2,11 @@ package com.example.firebase_auth;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +15,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProductList extends FirebaseRecyclerAdapter<DataModel,ProductList.ProductViewHolder>
 {
@@ -28,6 +36,80 @@ public class ProductList extends FirebaseRecyclerAdapter<DataModel,ProductList.P
         holder.price.setText(""+model.getProPrice());
         holder.des.setText(""+model.getProDes());
         Glide.with(holder.itemView.getContext()).load(model.getImgUrl()).into(holder.imageView);
+
+        holder.menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(view.getContext(), holder.menu);
+                popupMenu.getMenuInflater().inflate(R.menu.edit_menu,popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        if (menuItem.getItemId()==R.id.update)
+                        {
+                            DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+                            Query applesQuery = ref.child("Products").orderByChild("id").equalTo(model.id);
+                            applesQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    for (DataSnapshot list : snapshot.getChildren()) {
+                                        FirebaseDatabase database = FirebaseDatabase.getInstance(); // initializing object of database
+                                        DatabaseReference myRef = database.getReference("Products").push(); // Creating main (parent) reference
+                                        String id = myRef.getKey();
+
+                                        DataModel dataModel = new DataModel(id,"CPU","12345","RRR","https://play-lh.googleusercontent.com/8W4Ai-J22SM8Xzwy75_wXqnYPm04zbc0QpXm6dfKiS-VNDgPjeS3rtu_yymuGWmGP4w=s48-rw");
+
+                                        list.getRef().setValue(dataModel);
+                                        //list.getRef().removeValue();
+                                        notifyDataSetChanged();
+                                    }
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+
+                            });
+
+
+}                        if (menuItem.getItemId()==R.id.delete)
+                        {
+                            DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+                            Query applesQuery = ref.child("Products").orderByChild("id").equalTo(model.id);
+                            applesQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    for (DataSnapshot list : snapshot.getChildren()) {
+//                                        FirebaseDatabase database = FirebaseDatabase.getInstance(); // initializing object of database
+//                                        DatabaseReference myRef = database.getReference("Products").push(); // Creating main (parent) reference
+//                                        String id = myRef.getKey();
+
+                                        //DataModel dataModel = new DataModel(id,"CPU","12345","RRR","https://play-lh.googleusercontent.com/8W4Ai-J22SM8Xzwy75_wXqnYPm04zbc0QpXm6dfKiS-VNDgPjeS3rtu_yymuGWmGP4w=s48-rw");
+
+                                        //list.getRef().setValue(dataModel);
+                                        list.getRef().removeValue();
+                                        notifyDataSetChanged();
+                                    }
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+                        }
+                        return true;
+
+                    }
+
+                });
+                popupMenu.show();
+            }
+        });
     }
 
     @NonNull
@@ -39,7 +121,7 @@ public class ProductList extends FirebaseRecyclerAdapter<DataModel,ProductList.P
     }
 
     public class ProductViewHolder extends RecyclerView.ViewHolder {
-        ImageView imageView;
+        ImageView imageView,menu;
         TextView name,price,des;
         public ProductViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -47,6 +129,7 @@ public class ProductList extends FirebaseRecyclerAdapter<DataModel,ProductList.P
             name=itemView.findViewById(R.id.list_proname);
             price=itemView.findViewById(R.id.list_proprice);
             des=itemView.findViewById(R.id.list_prodes);
+            menu=itemView.findViewById(R.id.menu);
         }
     }
 }
